08b546233b5e2f34a98e0b6d7a4e051330def051
Diana Muñoz <91489788+DIANAIMC@users.noreply.github.com>
2022-06-03 23:18:52 -0500

    Update TareaExtra-Diana.sql

--- TareaExtra-Diana.sql
+++ TareaExtra-Diana.sql
@@ -1,7 +1,7 @@
---Una de las métricas para saber si un cliente es bueno, aparte de la suma y el promedio de sus pagos, es si tenemos 
---una progresión consistentemente creciente en los montos.
+--Una de las mÃ©tricas para saber si un cliente es bueno, aparte de la suma y el promedio de sus pagos, es si tenemos 
+--una progresiÃ³n consistentemente creciente en los montos.
 
---Debemos calcular para cada cliente su promedio mensual de deltas en los pagos de sus órdenes en la tabla 
+--Debemos calcular para cada cliente su promedio mensual de deltas en los pagos de sus Ã³rdenes en la tabla 
 --order_details en la BD de Northwind, es decir, la diferencia entre el monto total de una orden en tiempo t 
 --y el anterior en t-1, para tener la foto completa sobre el customer lifetime value de cada miembro de nuestra
 -- cartera.
@@ -20,8 +20,8 @@ deltas as (
 	from pagos p
 	window w as (partition by p.customer_id)
 )
---Agrupamos las deltas por cliente, año y mes, sacamos el promedio de las delta en el mes
-select d.company_name as nombre_empresa, extract(year from d.fecha) as año, extract(month from d.fecha) as mes, avg(d.delta) as promedio_mes_deltas
+--Agrupamos las deltas por cliente (lo identificamos por el nombre de la empresa), aÃ±o y mes, sacamos el promedio de las delta en el mes
+select d.company_name as nombre_empresa, extract(year from d.fecha) as aÃ±o, extract(month from d.fecha) as mes, avg(d.delta) as promedio_mes_deltas
 from deltas d
 group by d.company_name, extract(year from d.fecha), extract(month from d.fecha)
 order by 1,2,3;
